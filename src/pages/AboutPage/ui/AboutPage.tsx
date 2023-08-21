a071cafaf8c8d542abf74b8877385c5fcb52a6ae
BRIGHT <87952798+BrightBoakye@users.noreply.github.com>
2023-08-21 09:48:46 +0100

    Update AboutPage.tsx edited about page to subscription

--- src/pages/AboutPage/ui/AboutPage.tsx
+++ src/pages/AboutPage/ui/AboutPage.tsx
@@ -1,16 +1,71 @@
-import React from 'react'
-import { Page } from 'widgets/Page'
-import { CardText } from 'shared/ui/CardText/CardText'
+import React from 'react';
+import { Page } from 'widgets/Page';
+import pricingBackground from './pricingBackground.png';
 
 const AboutPage = () => {
-  return (
-    <Page>
-      <CardText
-        title='About Us'
-        text='Virid Future is a startup dedicated to revolutionizing the way we approach agriculture. We believe that the use of cutting-edge technologies like artificial intelligence and machine learning can help us achieve unprecedented levels of efficiency and sustainability in farming, without harming our planet.'
-      />
-    </Page>
-  )
-}
+  const planContainerStyle = {
+    display: 'flex',
+    flexDirection: 'column' as const,
+    alignItems: 'center',
+    margin: '1rem',
+    width: '100%',
+  };
 
-export default AboutPage
+  const planStyle = {
+    width: '80%',
+    padding: '1rem',
+    border: '1px solid #ccc',
+    borderRadius: '8px',
+    boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.1)',
+    marginBottom: '1rem',
+  };
+
+  const subscribeButtonStyle = {
+    display: 'inline-block',
+    padding: '10px 20px',
+    background: 'green', // Change to green background
+    color: 'white',
+    border: 'none',
+    borderRadius: '4px',
+    cursor: 'pointer',
+    fontWeight: 'bold', // Add this line to make the text bold
+  };
+
+  return (
+    <Page background={pricingBackground}>
+      <div style={planContainerStyle}>
+        <h2>Pricing Plans</h2>
+        <div style={planStyle}>
+          <h3>Basic Plan</h3>
+          <p>
+            Access to Virid Punk platform
+            <br />
+            Land cover classification insights
+            <br />
+            Land use analysis recommendations
+            <br />
+            Price: $29/month
+          </p>
+          {/* Replace the following with your actual Stripe Payment Link URL */}
+          <a href="https://buy.stripe.com/6oEcPubhu0zLcPC9AB" style={subscribeButtonStyle}>
+            Subscribe
+          </a>
+        </div>
+
+        {/* Placeholder for Advanced Plan */}
+        <div style={planStyle}>
+          <h3>Advanced Plan (Coming Soon)</h3>
+          <p>Stay tuned for more details!</p>
+        </div>
+
+        {/* Placeholder for Premium Plan */}
+        <div style={planStyle}>
+          <h3>Premium Plan (Coming Soon)</h3>
+          <p>Exciting features to be announced!</p>
+        </div>
+      </div>
+    </Page>
+  );
+};
+
+export default AboutPage;
