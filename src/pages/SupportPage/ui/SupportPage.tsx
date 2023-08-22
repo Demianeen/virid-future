d15741fb8d35d6cf745ff7d824678de3afff68b8
BRIGHT <87952798+BrightBoakye@users.noreply.github.com>
2023-08-22 17:57:43 +0100

    Update SupportPage.tsx updated support page more friendly with gmail link

--- src/pages/SupportPage/ui/SupportPage.tsx
+++ src/pages/SupportPage/ui/SupportPage.tsx
@@ -1,16 +1,25 @@
-import React from 'react'
-import { CardText } from 'shared/ui/CardText/CardText'
-import { Page } from 'widgets/Page'
+import React from 'react';
+import { Page } from 'widgets/Page';
 
 const SupportPage = () => {
   return (
     <Page>
-      <CardText
-        title='Support'
-        text='Find Support at viridfuture@gmail.com'
-      />
+      <div style={{ marginBottom: '20px' }}>
+        <h2 style={{ color: 'green' }}>Find Support</h2>
+        <p>
+          Need Help? We are Here for You!
+          Whether you have questions, feedback, or need assistance, our team is ready to help.
+        </p>
+        <p>
+          Reach out to us at{' '}
+          <a href="mailto:viridfuture@gmail.com" style={{ color: 'lightgreen' }}>
+            viridfuture@gmail.com
+          </a>
+          , and we will get back to you as soon as possible.
+        </p>
+      </div>
     </Page>
-  )
-}
+  );
+};
 
-export default SupportPage
+export default SupportPage;
