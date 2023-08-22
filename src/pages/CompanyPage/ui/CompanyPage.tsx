2ba42ab81d1480df8b8ec30d8c21d795cf74fd63
BRIGHT <87952798+BrightBoakye@users.noreply.github.com>
2023-08-22 16:50:30 +0100

    Update CompanyPage.tsx updated companypage to include virid future background

--- src/pages/CompanyPage/ui/CompanyPage.tsx
+++ src/pages/CompanyPage/ui/CompanyPage.tsx
@@ -1,16 +1,17 @@
-import React from 'react'
-import { Page } from 'widgets/Page'
-import { CardText } from 'shared/ui/CardText/CardText'
+import React from 'react';
+import { Page } from 'widgets/Page';
+import companyPageBackground from './companyPageBackground.png';
 
 const CompanyPage = () => {
   return (
-    <Page>
-      <CardText
-        title='Company'
-        text='Virid Future is a startup on a mission to revolutionize agriculture with cutting-edge AI technology. We are passionate about creating a sustainable future for our planet, and we believe that the key to achieving this is through the intelligent use of technology'
-      />
+    <Page background={companyPageBackground}>
+      <div className="company-content">
+        <p className="company-description">
+          Virid Future is a startup on a mission to revolutionize agriculture with cutting-edge AI technology. We are passionate about creating a sustainable future for our planet, and we believe that the key to achieving this is through the intelligent use of technology.
+        </p>
+      </div>
     </Page>
-  )
+  );
 }
 
-export default CompanyPage
+export default CompanyPage;
